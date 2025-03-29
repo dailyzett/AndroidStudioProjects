e790be3ec1ffb0519fed2d3240e9e628e7345a03
김세준 <dailyzett@gmail.com>
2025-03-30 02:52:44 +0900

    Art Space App: Initial project setup and UI implementation
    -   Created necessary project files and directories.
    -   Implemented basic UI components for displaying artwork.
    - Added `AnimationInfo` data class to manage artwork details.
    -   Added `ArtworkCard` composable to display a single piece of art.
    -   Added `ArtworkDisplay` composable to manage the display of art with previous and next buttons.
    -   Added `ActionButton` composable for button with text
    -   Added app icon
    -   Added strings for title and description.
    - Add resources for images

--- ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -4,13 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.artspace.model.AnimationInfo
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +40,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ArtSpaceTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ArtworkDisplay(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +50,130 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ArtworkDisplay(modifier: Modifier = Modifier) {
+
+    val animationInfoList: List<AnimationInfo> = AnimationInfo.list()
+    val index = remember { mutableIntStateOf(0) }
+
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize(),
+    ) {
+
+        Box(
+            modifier = Modifier.padding(
+                start = 12.dp,
+                end = 12.dp,
+                top = 64.dp,
+                bottom = 54.dp
+            )
+        ) {
+            ArtworkCard(
+                imageRes = animationInfoList[index.intValue].painterId,
+                contentDescription = null,
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .padding(
+                    start = 12.dp,
+                    end = 12.dp,
+                )
+                .background(color = Color.LightGray)
+                .fillMaxWidth(),
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = stringResource(animationInfoList[index.intValue].title),
+                )
+
+                Text(
+                    text = stringResource(animationInfoList[index.intValue].description),
+                )
+            }
+        }
+        
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            ActionButton(
+                onClick = {
+                    if (index.intValue > 0) {
+                        index.intValue--
+                    } else {
+                        index.intValue = animationInfoList.size - 1
+                    }
+                },
+                modifier = Modifier
+                    .width(150.dp),
+                text = "Previous",
+            )
+
+            ActionButton(
+                onClick = {
+                    if (index.intValue < animationInfoList.size - 1) {
+                        index.intValue++
+                    } else {
+                        index.intValue = 0
+                    }
+                },
+                modifier = Modifier
+                    .width(150.dp),
+                text = "Next",
+            )
+        }
+    }
+}
+
+@Composable
+private fun ActionButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        modifier
+    ) {
+        Text(text = text)
+    }
+}
+
+@Composable
+fun ArtworkCard(
+    imageRes: Int,
+    contentDescription: String?,
+) {
+    Box(
+        modifier = Modifier
+            .shadow(elevation = 4.dp, shape = RectangleShape)
+            .background(Color.White)
+            .padding(22.dp)
+    ) {
+        Column {
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(4f / 5f),
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+        ArtworkDisplay()
     }
 }
--- ArtSpace/app/src/main/java/com/example/artspace/model/AnimationInfo.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/model/AnimationInfo.kt
@@ -1,3 +1,32 @@
 package com.example.artspace.model
 
-data class AnimationInfo()
+import com.example.artspace.R
+
+data class AnimationInfo(
+    val painterId: Int,
+    val title: Int,
+    val description: Int,
+) {
+
+    companion object {
+        fun first(): AnimationInfo {
+            return AnimationInfo(
+                painterId = R.drawable.kaguya,
+                title = R.string.title_kaguya,
+                description = R.string.ch_kaguya,
+            )
+        }
+
+        fun second(): AnimationInfo {
+            return AnimationInfo(
+                painterId = R.drawable.milije,
+                title = R.string.title_milije,
+                description = R.string.ch_milije,
+            )
+        }
+
+        fun list(): List<AnimationInfo> {
+            return listOf(first(), second())
+        }
+    }
+}
