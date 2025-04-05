9f3246f82530b02fb11b36196d9b582af7141c84
김세준 <dailyzett@gmail.com>
2025-04-06 04:08:36 +0900

    Superheroes App: Add AnimatedVisibility and LazyColumn Enhancements
    -   Implemented `AnimatedVisibility` to apply an enter/exit animation to the entire `LazyColumn` in `HeroList`.
    -   Used `fadeIn` and `fadeOut` with `spring` animation for the `AnimatedVisibility` component.
    -   Utilized `slideInVertically` with `spring` to animate individual `HeroListItem` components as they enter the view, creating a sliding-in effect.
    -   Adjusted the initial offset of the `slideInVertically` animation to vary based on item index, making lower items in the list slide from further down.
    -   Replaced `items` with `itemsIndexed` in `HeroList` to enable animations based on the index.
    -   Implemented `CenterAlignedTopAppBar` for the app's top bar.
    -   Applied `MaterialTheme` to the app.
    - Added SuperheroesApp function with Scaffold.
    - Added HeroList component to display data from HeroesRepository.

--- Superheroes/app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ Superheroes/app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -1,6 +1,14 @@
 package com.example.superheroes
 
 import android.content.res.Configuration
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.Spring.DampingRatioLowBouncy
+import androidx.compose.animation.core.Spring.StiffnessVeryLow
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +20,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -19,6 +28,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -36,12 +46,45 @@ fun HeroList(
     heroes: List<Hero>,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn() {
-        items(heroes.size) {
-            HeroListItem(
-                hero = heroes[it],
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
-            )
+
+    //컴포넌트에 진입 애니메이션을 적용하기 위한 코드
+    val visibleSate = remember {
+        MutableTransitionState(false).apply {
+            targetState = true
+        }
+    }
+
+    //상위컨테이너 애니메이션, 리스트 전체를 한번에 나타나게 하거나 사라지게 하는 역할들 담당
+    AnimatedVisibility(
+        visibleState = visibleSate,
+        enter = fadeIn(
+            animationSpec = spring(dampingRatio = DampingRatioLowBouncy)
+        ),
+        exit = fadeOut(),
+        modifier = modifier
+    ) {
+        LazyColumn() {
+            itemsIndexed(heroes) { index, hero ->
+                HeroListItem(
+                    hero = hero,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                        //개별항목 애니메이션
+                        .animateEnterExit(
+                            enter = slideInVertically(
+                                // 각 항목이 아래에서 위로 슬라이딩하면서 나타나는 효과 적용
+                                animationSpec = spring(
+                                    stiffness = StiffnessVeryLow, // 스프링 강성을 매우 낮게 설정하여 부드러운 움직임을 만듦
+                                    dampingRatio = DampingRatioLowBouncy //약간의 튕김 효과가 있는 애니메이션 적용
+                                ),
+
+                                //각 항목의 시작 오프셋을 항목의 인덱스에 비례하게 설정
+                                //즉 리스트의 아래쪽 항목일수록 더 멀리서부터 슬라이딩하게됨
+                                initialOffsetY = { it * (index + 1) }
+                            )
+                        )
+                )
+            }
         }
     }
 }
--- Superheroes/app/src/main/java/com/example/superheroes/MainActivity.kt
+++ Superheroes/app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import com.example.superheroes.model.HeroesRepository
 import com.example.superheroes.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +24,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperheroesTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SuperheroesApp()
+                }
             }
         }
     }
+
+    @Composable
+    fun SuperheroesApp() {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = { TopAppBar() },
+        ) {
+            val heroes = HeroesRepository.heroes
+            HeroList(heroes = heroes, Modifier.padding(it))
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun TopAppBar(modifier: Modifier = Modifier) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            },
+            modifier = modifier
+        )
+    }
 }
+
