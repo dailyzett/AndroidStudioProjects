c6e90ea1330e37fcf6e79a069255395a505b5566
김세준 <dailyzett@gmail.com>
2025-04-19 00:35:31 +0900

    Mars 앱에 리포지토리 및 수동 DI 추가
    - MarsPhotosRepository 인터페이스와 NetworkMarsPhotosRepository 클래스 추가
    
    - MarsApiService 인터페이스를 정의하고 retrofitService를 통해 MarsApiService 인스턴스를 생성하여 활용하도록 수정
    
    - MarsViewModel에 MarsPhotosRepository 주입 및 ViewModelFactory를 통해 생성하도록 구현
    
    - MarsPhotosApplication 클래스 생성 및 Application에서 AppContainer 인스턴스 생성
    
    - AppContainer 인터페이스와 DefaultAppContainer 클래스를 생성하여 종속성 주입하도록 구현
    
    - MarsViewModel에서 NetworkMarsPhotosRepository 인스턴스 생성 방식 수정
    
    - MarsPhotosApp에서 viewModel 생성시 MarsViewModel.Factory를 사용하도록 수정
    
    - AndroidManifest.xml에 MarsPhotosApplication 이름 등록

--- mars-photos-starter/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ mars-photos-starter/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -19,12 +19,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
-import com.example.marsphotos.data.NetworkMarsPhotosRepository
-import com.example.marsphotos.network.MarsApi
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.marsphotos.MarsPhotosApplication
+import com.example.marsphotos.data.MarsPhotosRepository
 import com.example.marsphotos.model.MarsPhoto
 import kotlinx.coroutines.launch
-import okio.utf8Size
 import retrofit2.HttpException
 import java.io.IOException
 
@@ -34,7 +37,9 @@ sealed interface MarsUiState {
     object Loading : MarsUiState
 }
 
-class MarsViewModel : ViewModel() {
+class MarsViewModel(
+    private val marsPhotosRepository: MarsPhotosRepository
+) : ViewModel() {
     /** 가장 최근 요청의 상태를 저장하는 변경 가능한 상태입니다. */
     var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
         private set
@@ -55,7 +60,6 @@ class MarsViewModel : ViewModel() {
             marsUiState = MarsUiState.Loading
             marsUiState = try {
 
-                val marsPhotosRepository = NetworkMarsPhotosRepository()
                 val listResult = marsPhotosRepository.getMarsPhotos()
 
                 MarsUiState.Success(
@@ -65,6 +69,16 @@ class MarsViewModel : ViewModel() {
                 MarsUiState.Error
             } catch (e: HttpException) {
                 MarsUiState.Error
+            }
+        }
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as MarsPhotosApplication)
+                val marsPhotosRepository = application.container.marsPhotosRepository
+                MarsViewModel(marsPhotosRepository = marsPhotosRepository)
             }
         }
     }
