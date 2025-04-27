3733e6a33e6fe91dcd0b15dcd56a7da8324ee4d3
김세준 <dailyzett@gmail.com>
2025-04-27 12:04:13 +0900

    Flight Search 앱 신규 생성

---
+++ FlightSearch/app/src/main/java/com/example/flightsearch/ui/search/SearchScreen.kt
@@ -0,0 +1,15 @@
+package com.example.flightsearch.ui.search
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SearchScreen() {
+
+}
+
+@Composable
+@Preview(showBackground = false)
+fun SearchScreenPreview() {
+
+}
