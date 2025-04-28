c6941b35d30cfd4c02f208f5541a116b968ae076
김세준 <dailyzett@gmail.com>
2025-04-29 07:52:00 +0900

    BlurWorker 클래스에서 이미지 블러 처리 작업 구현 및 결과 알림 추가

--- workmanager-starter/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ workmanager-starter/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -1,9 +1,17 @@
 package com.example.bluromatic.workers
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.example.bluromatic.DELAY_TIME_MILLIS
 import com.example.bluromatic.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 private const val TAG = "BlurWorker"
 
@@ -14,6 +22,34 @@ class BlurWorker(context: Context, params: WorkerParameters) : CoroutineWorker(c
             context = applicationContext,
         )
 
-        TODO()
+        return withContext(Dispatchers.IO) {
+
+            delay(DELAY_TIME_MILLIS)
+
+            return@withContext try {
+                val picture = BitmapFactory.decodeResource(
+                    applicationContext.resources,
+                    R.drawable.android_cupcake
+                )
+
+                val output: Bitmap = blurBitmap(picture, 1)
+
+                val outputUrl: Uri = writeBitmapToFile(applicationContext, output)
+
+                makeStatusNotification(
+                    "Output is $outputUrl",
+                    applicationContext
+                )
+
+                Result.success()
+            } catch (throwable: Throwable) {
+                Log.e(
+                    TAG,
+                    applicationContext.resources.getString(R.string.error_applying_blur),
+                    throwable
+                )
+                Result.failure()
+            }
+        }
     }
 }
